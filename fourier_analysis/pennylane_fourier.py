ca8d7946ba295bcdde87fa2cb989bc4746c23dbb
Rodrigo Coelho <rodrigelho.com>
2023-03-15 17:12:44 +0000

    Added Fourier Analysis

--- fourier_analysis/pennylane_fourier.py
+++ fourier_analysis/pennylane_fourier.py
@@ -2,6 +2,10 @@
 import pennylane as qml
 from fourier_analysis.pennylane_operations import QMLOperations
 from pennylane.fourier import coefficients, circuit_spectrum
+from functools import partial
+from pennylane.fourier.visualize import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Fourier_Analysis():
     def __init__(self, num_qubits, num_layers, circuit_arch, data_reuploading, measurement, weights, data):
@@ -12,6 +16,11 @@ class Fourier_Analysis():
         self.measurement = measurement
         self.weights = weights
         self.data = data
+        if type(data) == int or type(data) == float:
+            self.data_dim = 1
+        else:
+            self.data_dim = len(data)
+        self.operations = QMLOperations()
         self.circuit = self.create_circuit()
 
         #The weights must have shape(num_layers, num_qubits*2)
@@ -20,6 +29,54 @@ class Fourier_Analysis():
         dev = qml.device("default.qubit", wires = self.num_qubits)
         if self.circuit_arch == "skolik":
             if self.data_reuploading == "schuld":
-                circuit = qml.QNode(QMLOperations().schuld_datareup(self.weights, self.data, self.num_layers), dev)
+                circuit = qml.QNode(self.operations.schuld_datareup, dev)
+            elif self.data_reuploading == "basic":
+                circuit = qml.QNode(self.operations.basic_datareup, dev)
         return circuit
-        
+    
+    def draw_circuit(self):
+        return print(qml.draw(self.circuit)(self.weights, self.num_layers, self.data_dim, self.data))
+    
+    def circuit_spectrum(self):
+        return circuit_spectrum(self.circuit)(self.weights, self.num_layers,self.data_dim, self.data)
+    
+    def fourier_coefficients(self, weights = None):
+        if weights is None:
+            partial_circuit = partial(self.circuit, self.weights, self.num_layers, self.data_dim)
+        else:
+            partial_circuit = partial(self.circuit, weights, self.num_layers, self.data_dim)
+        return coefficients(partial_circuit, self.data_dim, self.num_layers)
+    
+    def plot_coefficients(self, weights = None):
+        fig, ax = plt.subplots(2, 1, sharex=True, sharey=True) # Set up the axes
+        bar(self.fourier_coefficients(weights), self.data_dim, ax)
+
+    def plot_random_coefficients_distribution(self, dist_type = "violin"):
+        coeffs = []
+
+        for _ in range(100):
+            weights = np.random.uniform(0, 2*np.pi, size=(self.num_layers+1, self.num_qubits*2))
+            c = coefficients(partial(self.circuit, weights, self.num_layers, self.data_dim), self.data_dim, self.num_layers)
+            coeffs.append(np.round(c, decimals=8))
+
+        if dist_type == "violin":
+            fig, ax = plt.subplots(2, 1, sharey=True, figsize=(15, 4))
+            violin(coeffs, self.data_dim, ax, show_freqs=True)
+
+        elif dist_type == "box":
+            # The subplot axes must be *polar* for the radial plots
+            fig, ax = plt.subplots(
+                1, 2, sharex=True, sharey=True,
+                subplot_kw=dict(polar=True),
+                figsize=(15, 8)
+            )
+            radial_box(coeffs, 2, ax, show_freqs=True, show_fliers=False)
+
+        elif dist_type == "panel":
+            assert(self.data_dim <= 2)
+            # Need a grid large enough to hold all coefficients up to frequency 2
+            fig, ax = plt.subplots(5, 5, figsize=(12, 10), sharex=True, sharey=True)
+            panel(coeffs, 2, ax)
+
+
+
--- fourier_analysis/pennylane_operations.py
+++ fourier_analysis/pennylane_operations.py
@@ -5,7 +5,7 @@ class QMLOperations():
     def __init__(self):
         pass
 
-    def skolik_variational_layer(wires, params):
+    def skolik_variational_layer(self,wires, params):
         [[qml.RY(params[2*i], wires[i]),
          qml.RZ(params[(i*2)+1], wires[i])] for i in range(len(wires))]
     
@@ -14,13 +14,25 @@ class QMLOperations():
         if len(wires) != 2:
             qml.CZ(wires = [wires[0], wires[-1]])
     
-    def skolik_data_encoding(self,wires, data):
-        [qml.RX(data[i], wires[i], id = f"x_{i}") for i in range(len(wires))]
+    def skolik_data_encoding(self,wires, data, data_dim):
+        if data_dim == 1:
+            [qml.RX(data, wires[i], id = f"x_{i}") for i in range(len(wires))]
+        else:
+            [qml.RX(data[i], wires[i], id = f"x_{i}") for i in range(len(wires))]
 
-    def schuld_datareup(self, params, data, num_layers):
+    def schuld_datareup(self, params,num_layers, data_dim, data):
         for l in range(num_layers):
             self.skolik_variational_layer(range(4), params[l])
             self.skolik_entangling_layer(range(4))
-            self.skolik_data_encoding(range(4), data)
+            self.skolik_data_encoding(range(4), data, data_dim)
         self.skolik_variational_layer(range(4), params[num_layers])
+        return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
+    
+    def basic_datareup(self, params,num_layers,data_dim, data):
+        for l in range(num_layers-1):
+            self.data_encoding(range(4), data, data_dim)
+            self.variational_layer(range(4), params[l])
+            self.entangling_layer(range(4))
+        self.data_encoding(range(4), data, data_dim)
+        self.variational_layer(range(4), params[num_layers-1])
         return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))

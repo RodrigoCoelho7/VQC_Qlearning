f0cfb5c30df08fc673912d0c2b472f04d2bd1eb2
Rodrigo Coelho <rodrigelho.com>
2023-05-16 22:56:34 +0100

    update

--- configs/acrobot-v1/continuous/uqc/localexpectation_outscaling/optuna_search.py
+++ configs/acrobot-v1/continuous/uqc/localexpectation_outscaling/optuna_search.py
@@ -6,7 +6,8 @@ from DQN.operators import Max
 from vqc.vqc_circuits import UQC
 from model.output_scaling import LocalExpectationRescaling
 import tensorflow as tf
-from collections import deque
+from vqc.data_reup_model import UniversalQuantumClassifier
+from wrappers import ContinuousEncoding
 
 #circuit_arch = "skolik", "lock" or "uqc"
 #data_reuploading = "baseline", "basic" or "schuld"
@@ -24,15 +25,16 @@ ops = [cirq.Z(qubits[0]), cirq.X(qubits[0]), cirq.Y(qubits[0])]
 observables = [ops[0], ops[1], ops[2]]
 rescaling_type = LocalExpectationRescaling
 state_dim = 6
+pqc = UniversalQuantumClassifier
 
 # Parameters for the training
 gamma = 0.99
-steps_per_update = 5
+#steps_per_update = 5
 batch_size = 32
 #steps_per_target_update = 1
 #max_memory_length = 70000
 #replay_memory = deque(maxlen=max_memory_length)
-num_episodes = 250
+num_episodes = 200
 epsilon = 1.0  # Epsilon greedy parameter
 epsilon_min = 0.01  # Minimum epsilon greedy parameter
 decay_epsilon = 0.99 # Decay rate of epsilon greedy parameter
@@ -54,7 +56,7 @@ w_out = 3
 
 #Choose the environment
 environment = "Acrobot-v1"
-input_encoding = "continuous"
+input_encoding = ContinuousEncoding
 early_stopping = False
 acceptance_reward = -100
 necessary_episodes = 25
--- optuna_hypersearch_acrobot.py
+++ optuna_hypersearch_acrobot.py
@@ -8,6 +8,8 @@ import optuna
 from collections import deque
 import pickle
 import logging
+from wrappers import ContinuousEncoding, NothingEncoding
+from vqc.vqc_circuits import UQC
 
 if __name__ == "__main__":
     seed = 42
@@ -19,20 +21,23 @@ if __name__ == "__main__":
 
     import_name = path_to_file.rsplit('/', 1)[1][:-3]
 
-    script = __import__(import_name)
-
-    def train_agent(steps_per_target_update, max_memory_length, learning_rate_in, learning_rate_var):
+    def train_agent(steps_per_update,steps_per_target_update, max_memory_length, learning_rate_in, learning_rate_var, input_encoding, num_layers):
             from model.q_learning_agent import QLearningAgent
             from DQN.dqn import DQN
             import tensorflow as tf
             tf.random.set_seed(seed)
 
+            script = __import__(import_name)
+
+            vqc = UQC(script.num_qubits, num_layers)
+
+
             #Create the models
-            model = QLearningAgent(script.vqc, script.observables, False, script.state_dim, script.rescaling_type, script.activation)
-            model_target = QLearningAgent(script.vqc, script.observables, True, script.state_dim, script.rescaling_type, script.activation)
+            model = QLearningAgent(vqc, script.observables, False, script.state_dim, script.rescaling_type, script.activation, script.pqc)
+            model_target = QLearningAgent(vqc, script.observables, True, script.state_dim, script.rescaling_type, script.activation, script.pqc)
             model_target.set_weights(model.get_weights())
 
-            steps_per_target_update = steps_per_target_update * script.steps_per_update
+            steps_per_target_update = steps_per_target_update * steps_per_update
             optimizer_in = tf.keras.optimizers.Adam(learning_rate = learning_rate_in, amsgrad = True)
             optimizer_var = tf.keras.optimizers.Adam(learning_rate = learning_rate_var, amsgrad = True)
             optimizer_bias = tf.keras.optimizers.Adam(learning_rate = learning_rate_var, amsgrad = True)
@@ -41,8 +46,8 @@ if __name__ == "__main__":
             # Create the agent
             agent = DQN(model, model_target, script.gamma, script.num_episodes, max_memory_length,
                         replay_memory, script.policy, script.batch_size,
-                        script.steps_per_update, steps_per_target_update, optimizer_in, script.optimizer_out, optimizer_var,
-                        optimizer_bias, script.w_in, script.w_var, script.w_out,script.w_bias, script.input_encoding, script.early_stopping,
+                        steps_per_update, steps_per_target_update, optimizer_in, script.optimizer_out, optimizer_var,
+                        optimizer_bias, script.w_in, script.w_var, script.w_out,script.w_bias, input_encoding, script.early_stopping,
                         script.operator)
 
             agent.train(script.environment, script.num_actions, script.acceptance_reward, script.necessary_episodes)
@@ -50,11 +55,14 @@ if __name__ == "__main__":
             return agent.episode_reward_history
     
     def sample_model_params(trial:optuna.Trial):
-        steps_per_target_update = trial.suggest_categorical("steps_per_target_update", [5,25,50,100])
-        max_memory_length = trial.suggest_categorical("max_memory_length", [25000,50000,100000])
+        steps_per_update = trial.suggest_categorical("steps_per_update", [3,5,10,20])
+        steps_per_target_update = trial.suggest_categorical("steps_per_target_update", [5,25,100,500,1000])
+        max_memory_length = trial.suggest_categorical("max_memory_length", [10000,25000,50000,100000])
         learning_rate_in = trial.suggest_categorical("learning_rate_in", [0.0001,0.001])
         learning_rate_var = trial.suggest_categorical("learning_rate_var", [0.0001,0.001])
-        return  steps_per_target_update, max_memory_length, learning_rate_in, learning_rate_var
+        input_encoding = trial.suggest_categorical("input_encoding", [ContinuousEncoding, NothingEncoding])
+        num_layers = trial.suggest_categorical("num_layers", [5,6,7,8])
+        return  steps_per_update, steps_per_target_update, max_memory_length, learning_rate_in, learning_rate_var, input_encoding, num_layers
     
     def objective_function(results):
         results_mean = np.mean(results, axis=0)
@@ -77,9 +85,9 @@ if __name__ == "__main__":
 
     optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
     sampler = optuna.samplers.TPESampler(seed=seed)
-    study_name = "1qubituqc_acrobot_test"
+    study_name = "1qubituqc_acrobot_complete"
     storage_name = "sqlite:///{}.db".format(study_name)
-    study = optuna.create_study(direction="minimize", sampler = sampler,study_name=study_name, storage=storage_name, pruner = optuna.pruners.HyperbandPruner(), load_if_exists=True)
+    study = optuna.create_study(direction="minimize", sampler = sampler,study_name=study_name, storage=storage_name, load_if_exists=True)
     study.optimize(objective, n_trials=500)
 
     print("Number of finished trials: {}".format(len(study.trials)))

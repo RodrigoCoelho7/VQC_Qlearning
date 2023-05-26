7b78d25aee20d808648906d106f266b0647edb4a
Rodrigo Coelho <rodrigelho.com>
2023-05-26 11:40:48 +0100

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-python main.py configs/continuous/uqc/globalskolik_outscaling/XZ_obs/multi_qubit_full_encoding_final/2qubits_5layers.py
-python main.py configs/continuous/uqc/globalskolik_outscaling/XZ_obs/multi_qubit_full_encoding_final/4qubits_5layers.py
-python main.py configs/continuous/uqc/globalskolik_outscaling/XZ_obs/skolik_actual_optimal_hyper_mse.py
+python main.py configs/poster/2qubits_5layers.py
+python main.py configs/poster/4qubits_5layers.py
+python main.py configs/poster/2qubits_5layers.py
+python main.py configs/poster/4qubits_5layers.py

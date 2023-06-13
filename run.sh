ed6b9d13f719c7ec957d4a65ecd2e41d37b9cb83
Rodrigo <rodrigelho@gmail.com>
2023-06-13 15:01:08 +0100

    Studied entanglement on multi-qubit UQC

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
+python main.py configs/uqc_entanglement/4_qubit_uqc_noentangling.py
 python main.py configs/uqc_entanglement/2_qubit_uqc_cnot.py
 python main.py configs/uqc_entanglement/2_qubit_uqc_cz.py
 python main.py configs/uqc_entanglement/2_qubit_uqc_noentangling.py
 python main.py configs/uqc_entanglement/4_qubit_uqc_cnot.py
 python main.py configs/uqc_entanglement/4_qubit_uqc_cz.py
 python main.py configs/uqc_entanglement/4_qubit_uqc_noentangling.py
-python main.py configs/uqc_entanglement/2_qubit_uqc_cnot.py
-python main.py configs/uqc_entanglement/2_qubit_uqc_cz.py
-python main.py configs/uqc_entanglement/2_qubit_uqc_noentangling.py

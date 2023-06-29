e5218fabaa283f26ad0812b0683a80819cb66f10
Rodrigo <rodrigelho@gmail.com>
2023-06-29 22:17:18 +0100

    models to run

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-python main.py configs/uqc_encoding/4qubits_continuous_encoding.py
-python main.py configs/uqc_encoding/4qubits_nothing_encoding.py
-python main.py configs/uqc_encoding/4qubits_scaled_continuous_encoding.py
+python main.py configs/thesis_organized/1.1/skolik_datareup.py
+python main.py configs/thesis_organized/1.1/skolik_baseline.py
+python main.py configs/thesis_organized/1.1/skolik_datareup_no_in.py
+python main.py configs/thesis_organized/1.1/skolik_baseline_no_in.py
+python main.py configs/thesis_organized/1.1/skolik_datareup_no_out.py
+python main.py configs/thesis_organized/1.1/skolik_baseline_no_out.py
+python main.py configs/thesis_organized/1.1/skolik_datareup_no_in_out.py
+python main.py configs/thesis_organized/1.1/skolik_baseline_no_in_out.py

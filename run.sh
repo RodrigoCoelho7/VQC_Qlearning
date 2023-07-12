8ad9aa1517ca4abf2f29bca8ab5de3379e51d868
Rodrigo <rodrigelho@gmail.com>
2023-07-12 14:37:35 +0100

    run different models

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-python main.py configs/uqc_test/uqc_1qubit_XY_nothing.py
-python main.py configs/uqc_test/uqc_1qubit_ZX_nothing.py
-python main.py configs/uqc_test/uqc_1qubit_ZY_continuous.py
-python main.py configs/uqc_test/uqc_1qubit_ZY_nothing.py
+python main.py configs/thesis_organized/1.2/uqc_1qubit_XY_nothing.py
+python main.py configs/thesis_organized/1.2/uqc_1qubit_ZX_continuous.py
+python main.py configs/thesis_organized/1.2/uqc_1qubit_ZX_nothing.py
+python main.py configs/thesis_organized/1.2/uqc_1qubit_ZY_continuous.py
+python main.py configs/thesis_organized/1.2/uqc_1qubit_ZY_nothing.py

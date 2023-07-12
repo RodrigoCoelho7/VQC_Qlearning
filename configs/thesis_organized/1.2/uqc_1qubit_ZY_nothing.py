46a1c4974bfb5db6e9dc1d1091ae00f6ca2c5c11
Rodrigo <rodrigelho@gmail.com>
2023-07-12 14:37:11 +0100

    Changed hyperparameters so that they match the ones of the skolik architecture

--- configs/thesis_organized/1.2/uqc_1qubit_ZY_nothing.py
+++ configs/thesis_organized/1.2/uqc_1qubit_ZY_nothing.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import cirq
 from collections import deque
 from vqc.vqc_circuits import UQC
-from model.output_scaling import GlobalSkolikRescaling
+from model.output_scaling import LocalSkolikRescaling
 from DQN.policies import EGreedyExpStrategy
 from DQN.operators import Max
 from wrappers import NothingEncoding
@@ -23,7 +23,7 @@ vqc = UQC(num_qubits, num_layers)
 qubits = cirq.GridQubit.rect(1, num_qubits)
 ops = [cirq.Z(qubits[0]), cirq.Y(qubits[0])]
 observables = [ops[0], ops[1]]
-rescaling_type = GlobalSkolikRescaling
+rescaling_type = LocalSkolikRescaling
 state_dim = 4
 
 # Parameters for the training
@@ -37,10 +37,11 @@ decay_epsilon = 0.99 # Decay rate of epsilon greedy parameter
 policy = EGreedyExpStrategy(epsilon, epsilon_min, decay_epsilon)
 batch_size = 16
 steps_per_update = 1 # Train the model every x steps
-steps_per_target_update = 5 # Update the target model every x steps
+steps_per_target_update = 1 # Update the target model every x steps
 operator = Max()
 activation = "linear"
 parameters_relative_change = True
+entanglement_study = False
 
 # Prepare the optimizers
 learning_rate_in = 0.001

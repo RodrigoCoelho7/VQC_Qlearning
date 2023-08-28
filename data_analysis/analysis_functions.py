8587f806adf763e2d7c3e946b764b2b4842cf453
Rodrigo <rodrigelho@gmail.com>
2023-08-28 14:36:49 +0100

    Get steps per episode

--- data_analysis/analysis_functions.py
+++ data_analysis/analysis_functions.py
@@ -61,6 +61,9 @@ class Analysis():
     def get_parameters_relative_change(self):
         return [self.data[i]["parameters_relative_change_array"] for i in range(self.number_of_agents)]
 
+    def get_steps_per_episode(self):
+        return [self.data[i]["steps_per_episode"] for i in range(self.number_of_agents)]
+
     def get_moving_average(self, window_size):
         rewards = self.get_rewards()
         moving_averages = []
